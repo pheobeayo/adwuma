2fd1059ebcf03a2b461ae00d5487cf07ff277509
Ifeoluwa Sanni <47613365+pheobeayo@users.noreply.github.com>
2024-06-19 20:28:02 +0000

    modify:splash

--- components/onboarding/Onboarding.tsx
+++ components/onboarding/Onboarding.tsx
@@ -14,7 +14,7 @@ const Onboarding = ({ onboarded, setOnboarded }: Props) => {
 
   setTimeout(function () {
     setTimePassed(true);
-  }, 5000); //For a little delay
+  }, 10000); //For a little delay
 
   const handleSubmit = () => {
     setOnboarded(true);
